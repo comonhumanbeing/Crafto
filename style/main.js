e6edd6d059fe7e78edadf9d9d50b76a55abc580a
comonhumanbeing <leo_than@hotmail.fr>
2021-02-05 18:40:46 +0100

    commit1

---
+++ style/main.js
@@ -0,0 +1,10 @@
+	// Scrolly.
+		$('.scrolly-middle').scrolly({
+			speed: 1000,
+			anchor: 'middle'
+		});
+
+		$('.scrolly').scrolly({
+			speed: 1000,
+			offset: function() { return (breakpoints.active('<=mobile') ? 70 : 190); }
+		});
